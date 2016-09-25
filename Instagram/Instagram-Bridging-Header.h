4027d719043a60774c80c0db9aa898e12875f525
tyoko <tamoriinu@gmail.com>
2016-09-26 00:19:49 +0900

    chap 7.6  end

---
+++ Instagram/Instagram-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Instagram-Bridging-Header.h
+//  Instagram
+//
+//  Created by tyoko on 2016/09/26.
+//  Copyright © 2016年 TakayoshiYokoyama. All rights reserved.
+//
+
+#ifndef Instagram_Bridging_Header_h
+#define Instagram_Bridging_Header_h
+
+#import <AdobeCreativeSDKCore/AdobeCreativeSDKCore.h>
+#import <AdobeCreativeSDKImage/AdobeCreativeSDKImage.h>
+
+#endif /* Instagram_Bridging_Header_h */
