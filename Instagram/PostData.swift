f3c42a79cf115240dd97df2e66f9b253176f5d7d
tyoko <tamoriinu@gmail.com>
2016-09-28 00:39:56 +0900

    chap 9.1 end

---
+++ Instagram/PostData.swift
@@ -0,0 +1,48 @@
+//
+//  PostData.swift
+//  Instagram
+//
+//  Created by tyoko on 2016/09/28.
+//  Copyright © 2016年 TakayoshiYokoyama. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseDatabase
+
+class PostData: NSObject {
+    var id: String?
+    var image: UIImage?
+    var imageString: String?
+    var name: String?
+    var caption: String?
+    var date: NSDate?
+    var likes: [String] = []
+    var isLiked: Bool = false
+    
+    init(snapshot: FIRDataSnapshot, myId: String) {
+        id = snapshot.key
+        
+        let valueDictionary = snapshot.value as! [String: AnyObject]
+        
+        imageString = valueDictionary["image"] as? String
+        image = UIImage(data: NSData(base64EncodedString: imageString!, options: .IgnoreUnknownCharacters)!)
+        
+        name = valueDictionary["name"] as? String
+        
+        caption = valueDictionary["caption"] as? String
+        
+        if let likes = valueDictionary["likes"] as? [String] {
+            self.likes = likes
+        }
+        
+        for likeId in likes {
+            if likeId == myId {
+                isLiked = true
+                break
+            }
+        }
+        
+        self.date = NSDate(timeIntervalSinceReferenceDate: valueDictionary["time"] as! NSTimeInterval)
+    }
+}
