bc3e5a42a3fbabb711f993641551b01c7f748d48
tyoko <tamoriinu@gmail.com>
2016-09-29 08:25:59 +0900

    chap 9.2 UI setting end

---
+++ Instagram/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  Instagram
+//
+//  Created by tyoko on 2016/09/28.
+//  Copyright © 2016年 TakayoshiYokoyama. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
