540403c9ba687fea7902d8410df8a934bdd1ad81
tyoko <tamoriinu@gmail.com>
2016-09-30 11:52:48 +0900

    chap 9.3 end

--- Instagram/HomeViewController.swift
+++ Instagram/HomeViewController.swift
@@ -7,13 +7,28 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var postArray: [PostData] = []
+    
+    //FIRDatabaseのobserverEventの登録状態を表す
+    var observing = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        let nib = UINib(nibName: "PostTableViewCell", bundle: nil)
+        tableView.registerNib(nib, forCellReuseIdentifier: "Cell")
+        tableView.rowHeight = UITableViewAutomaticDimension
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +36,123 @@ class HomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if FIRAuth.auth()?.currentUser != nil {
+            if observing == false {
+                //要素が追加されたらpostArrayに追加してTableViewを再表示する
+                FIRDatabase.database().reference().child(CommonConst.PostPATH).observeEventType(.ChildChanged, withBlock: {snapshot in
+                    if let uid = FIRAuth.auth()?.currentUser?.uid{
+                        //PostDataクラスを生成して受け取ったデータを設定する
+                        let postData = PostData(snapshot: snapshot, myId: uid)
+                        //保持している配列からidが同じものを探す
+                        var index: Int = 0
+                        for post in self.postArray {
+                            if post.id == postData.id {
+                                index = self.postArray.indexOf(post)!
+                                break
+                            }
+                        }
+                        
+                        //差し替えるため一度削除
+                        self.postArray.removeAtIndex(index)
+                        
+                        //削除した所に更新済みのデータを追加する
+                        self.postArray.insert(postData, atIndex: index)
+                        
+                        //TableViewの現在表示されているセルを更新する
+                        self.tableView.reloadData()
+                        
+                    }
+                    
+                })
+                
+                //FIRDatabaseのobserverEventが上記コードにより登録されたため
+                //trueとする
+                observing = true
+            }
+        }else{
+            if observing == true {
+                //ログアウトを検出したら、一旦テーブルをクリアしてオブザーバーを削除する。
+                //テーブルをクリアする
+                postArray = []
+                tableView.reloadData()
+                //オブザーバーを削除する
+                FIRDatabase.database().reference().removeAllObservers()
+                
+                //FIRDatabaseのobserverEventが上記コードにより削除されたため
+                //falseとする
+                observing = false
+            }
+        }
     }
-    */
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return postArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        // セルを取得してデータを設定する
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! PostTableViewCell
+        cell.setPostData(postArray[indexPath.row])
+        
+        // セル内のボタンのアクションをソースコードで設定する
+        cell.likeButton.addTarget(self, action:#selector(handleButton(_:event:)), forControlEvents:  UIControlEvents.TouchUpInside)
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        // Auto Layoutを使ってセルの高さを動的に変更する
+        return UITableViewAutomaticDimension
+    }
 
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // セルをタップされたら何もせずに選択状態を解除する
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    // セル内のボタンがタップされた時に呼ばれるメソッド
+    func handleButton(sender: UIButton, event:UIEvent) {
+        
+        // タップされたセルのインデックスを求める
+        let touch = event.allTouches()?.first
+        let point = touch!.locationInView(self.tableView)
+        let indexPath = tableView.indexPathForRowAtPoint(point)
+        
+        // 配列からタップされたインデックスのデータを取り出す
+        let postData = postArray[indexPath!.row]
+        
+        // Firebaseに保存するデータの準備
+        if let uid = FIRAuth.auth()?.currentUser?.uid {
+            if postData.isLiked {
+                // すでにいいねをしていた場合はいいねを解除するためIDを取り除く
+                var index = -1
+                for likeId in postData.likes {
+                    if likeId == uid {
+                        // 削除するためにインデックスを保持しておく
+                        index = postData.likes.indexOf(likeId)!
+                        break
+                    }
+                }
+                postData.likes.removeAtIndex(index)
+            } else {
+                postData.likes.append(uid)
+            }
+            
+            let imageString = postData.imageString
+            let name = postData.name
+            let caption = postData.caption
+            let time = (postData.date?.timeIntervalSinceReferenceDate)! as NSTimeInterval
+            let likes = postData.likes
+            
+            // 辞書を作成してFirebaseに保存する
+            let post = ["caption": caption!, "image": imageString!, "name": name!, "time": time, "likes": likes]
+            let postRef = FIRDatabase.database().reference().child(CommonConst.PostPATH)
+            postRef.child(postData.id!).setValue(post)
+        }
+    }
+    
 }
--- Instagram/PostTableViewCell.swift
+++ Instagram/PostTableViewCell.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class PostTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var likeLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -20,5 +28,32 @@ class PostTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+    
+    func setPostData(postData: PostData){
+        postImageView.image = postData.image
+        captionLabel.text = "\(postData.name!) : \(postData.caption)"
+        let likeNumber = postData.likes.count
+        likeLabel.text = "\(likeNumber)"
+        
+        let formatter = NSDateFormatter()
+        formatter.locale = NSLocale(localeIdentifier: "ja_JP")
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        
+        let dateString:String = formatter.stringFromDate(postData.date!)
+        dateLabel.text = dateString
+        
+        if postData.isLiked {
+            let buttonImage = UIImage(named: "like_exist")
+            likeButton.setImage(buttonImage, forState: UIControlState.Normal)
+        }else{
+            let buttonImage = UIImage(named: "like_none")
+            likeButton.setImage(buttonImage, forState: UIControlState.Normal)
+        }
+    }
+    
+    
+    
+    
     
 }
